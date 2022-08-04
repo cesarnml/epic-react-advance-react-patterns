a583736009793ef7f0e9f47c04cae1bdd2f30fb3
Cesar Napoleon Mejia <cmejia@gmail.com>
2022-08-04 15:50:09 +0800

    Complete lesson 01

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -74,10 +74,17 @@ function useUser() {
 }
 
 // 🐨 add a function here called `updateUser`
+function updateUser(dispatch, user, updates) {
+  dispatch({type: 'start update', updates})
+  userClient.updateUser(user, updates).then(
+    updatedUser => dispatch({type: 'finish update', updatedUser}),
+    error => dispatch({type: 'fail update', error}),
+  )
+}
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
-// export {UserProvider, useUser}
+// export {UserProvider, useUser, updateUser}
 
 // src/screens/user-profile.js
 // import {UserProvider, useUser} from './context/user-context'
@@ -98,11 +105,7 @@ function UserSettings() {
   function handleSubmit(event) {
     event.preventDefault()
     // 🐨 move the following logic to the `updateUser` function you create above
-    userDispatch({type: 'start update', updates: formState})
-    userClient.updateUser(user, formState).then(
-      updatedUser => userDispatch({type: 'finish update', updatedUser}),
-      error => userDispatch({type: 'fail update', error}),
-    )
+    return updateUser(userDispatch, user, formState)
   }
 
   return (
--- src/my-notes.md
+++ src/my-notes.md
@@ -6,6 +6,7 @@
 
 - *The Context Module Functions Pattern allows you to encapsulate a complex set of state changes into a utility function which can be tree-shaken and lazily loaded*
 - Hot tip: `Context.displayName = 'MyContext'` to improve discoverability in DevTools
+- Avoids the need to wrap exported helper function in `useCallback` and removes state update complexity from within component logic by encapsulating it in helper methods that are passed dispatch and data needed to update context state
 
 ### Lesson 02 - Compound Components
 
